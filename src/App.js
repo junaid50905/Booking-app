64e647860f117e5275c082d71be3bf367a1f1afa
junaid hossain <junaidhossain50905@gmail.com>
2022-10-08 19:35:12 +0600

    push booking app to github

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+      <h1>Booking app</h1>
+      <br /><br /><br /><br /><br /><br />
+    </div>
+  );
+}
+
+export default App;
