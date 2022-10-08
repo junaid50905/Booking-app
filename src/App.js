aa135c6fe48fa1b9bdce31f8a10c146b56b29848
junaid hossain <junaidhossain50905@gmail.com>
2022-10-08 20:02:58 +0600

    booking app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
